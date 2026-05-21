856132655ad42afd93630977d20a0c3b1cff034b
Herek1 <herekkacper@gmail.com>
2026-05-21 12:31:46 +0200

    fix gui for alerts

--- src/main/java/client/Views/ShowAlert.java
+++ src/main/java/client/Views/ShowAlert.java
@@ -3,18 +3,34 @@ package client.Views;
 import javafx.application.Platform;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogPane;
 
 public class ShowAlert {
-    public static void error(String message) {
+
+    private static void show(Alert.AlertType type, String title, String message) {
+
         Platform.runLater(() -> {
-            Alert alert = new Alert(Alert.AlertType.ERROR, message, ButtonType.OK);
+
+            Alert alert = new Alert(type, message, ButtonType.OK);
+
+            alert.setTitle(title);
+            alert.setHeaderText(null);
+
+            DialogPane pane = alert.getDialogPane();
+
+            pane.getStylesheets().add(ShowAlert.class.getResource("/main/passwordmanager/style.css").toExternalForm());
+            pane.getStyleClass().add("dialog-pane");
+
             alert.showAndWait();
         });
     }
+
+    public static void error(String message) {
+        show(Alert.AlertType.ERROR, "Error", message);
+    }
     public static void info(String message) {
         Platform.runLater(() -> {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION, message, ButtonType.OK);
-            alert.showAndWait();
+            show(Alert.AlertType.INFORMATION, "Information", message);
         });
     }
 }
